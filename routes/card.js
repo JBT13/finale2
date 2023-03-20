55ab0eb33d4840a28bbfcc01e30cb6df35b19003
Jeremias Borjas Tablante - FB <FB687753@fb.is>
2023-03-20 12:09:37 +0000

    day 45

--- routes/card.js
+++ routes/card.js
@@ -27,40 +27,6 @@ router.post('/', (req, res) => {
     }
 });
 
-const app = express();
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-  const values = [
-    { name: 'Ace', value: 1 },
-    { name: '2', value: 2 },
-    { name: '3', value: 3 },
-    { name: '4', value: 4 },
-    { name: '5', value: 5 },
-    { name: '6', value: 6 },
-    { name: '7', value: 7 },
-    { name: '8', value: 8 },
-    { name: '9', value: 9 },
-    { name: '10', value: 10 },
-    { name: 'Jack', value: 11 },
-    { name: 'Queen', value: 12 },
-    { name: 'King', value: 13 },
-  ];
-
-  const deck = [];
-
-  for (const suit of suits) {
-    for (const value of values) {
-      deck.push({
-        suit: suit,
-        name: value.name,
-        value: value.value,
-      });
-    }
-  }
-
-  res.render('index', { deck });
-});
 
 module.exports = router;
